0f5687b88ad84dd298891a7e4cd5426b8af60f22
Vasyl Dovhan <vdovhan23@gmail.com>
2020-02-21 23:36:28 +0200

    #4

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+      <h1 class="font-medium text-4xl">Snippets</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur laborum maiores optio quae. Deleniti est iusto, nam odit officia possimus quo soluta ut. Accusantium est libero molestias velit! Enim.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
