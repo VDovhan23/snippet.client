1e59b95c75f47d9053cd8718822a0889d12248a5
VD_Styles <vdovhan23@gmail.com>
2020-03-12 23:23:36 +0200

    #32 Add step after and before api side

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
       <h1 class="font-medium text-4xl">Snippets</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur laborum maiores optio quae. Deleniti est iusto, nam odit officia possimus quo soluta ut. Accusantium est libero molestias velit! Enim.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Assumenda consequatur laborum maiores optio quae. Deleniti est iusto, nam odit officia possimus quo soluta ut. Accusantium est libero molestias velit! Enim.</p>
   </div>
 </template>
 
