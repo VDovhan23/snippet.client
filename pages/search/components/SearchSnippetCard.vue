3ae79dd483b4d201b6a5388a8062bd9a193ddf78
VD_Styles <vdovhan23@gmail.com>
2020-04-17 21:11:22 +0300

    #53 algolia components connect

---
+++ pages/search/components/SearchSnippetCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <snippet-card
+
+    :snippet="snippet"
+  >
+    <nuxt-link
+      :to="{}"
+    >
+      {{snippet.author.data.name}}
+    </nuxt-link>
+
+  </snippet-card>
+</template>
+
+<script>
+import SnippetCard from "../../../components/Snippets/SnippetCard";
+  export default {
+    props: {
+      snippet: {
+        required: true,
+        type: Object
+      },
+    },
+    components: {SnippetCard},
+    name: "SearchSnippetCard",
+  }
+</script>
+
+<style scoped>
+
+</style>
+
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -3,31 +3,79 @@
     <div>
       <div class="bg-white mb-16">
         <div class="container py-10 pb-16">
-          <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4"> Search</h1>
+          <div class="flex justify-between text-gray-600 mb-6 items-center">
+
+            <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4">
+              Search
+            </h1>
+
+            <client-only>
+              <AisPoweredBy/>
+            </client-only>
+
+          </div>
+          <ais-search-box
+            placeholder="Search snippets"
+            :class-names="{
+              'ais-SearchBox': 'w-full',
+              'ais-SearchBox-input': 'w-full border-2 border-gray-200 rounded-lg block p-4 text-lg',
+              'ais-SearchBox-submit': 'hidden',
+              'ais-SearchBox-submitIcon': 'hidden',
+              'ais-SearchBox-reset': 'hidden'
+            }"
+          />
         </div>
       </div>
 
 
-      <div class="container mt-16">
+      <div class="container">
+        <AisStateResults>
+          <div slot-scope="{query}">
+            <template v-if="query.length">
+              <AisStats>
+                <h1
+                  slot-scope="{nbHits}"
+                  class="text-gray-600 text-xl font-medium mb-6">
+                  Snippets ({{nbHits}})
+                </h1>
+              </AisStats>
 
-        <h1 class="text-gray-600 text-xl font-medium mb-6">Snippets </h1>
-
-
+              <ais-hits>
+                <div slot="item" slot-scope="{item}">
+                  <SearchSnippetCard :snippet="item.data"/>
+                </div>
+              </ais-hits>
+            </template>
+          </div>
+        </AisStateResults>
       </div>
-
-
     </div>
   </AisInstantSearchSsr>
 </template>
 
 <script>
   import {
-    AisInstantSearchSsr
+    AisHits,
+    AisInstantSearchSsr,
+    AisSearchBox,
+    AisPoweredBy,
+    AisStats,
+    AisStateResults
   } from 'vue-instantsearch'
+
+  import SearchSnippetCard from './components/SearchSnippetCard.vue'
 
   export default {
     name: "index",
-    components: {AisInstantSearchSsr},
+    components: {
+      SearchSnippetCard,
+      AisHits,
+      AisInstantSearchSsr,
+      AisSearchBox,
+      AisPoweredBy,
+      AisStats,
+      AisStateResults
+    },
     head() {
       return {
         title: 'Search',
@@ -35,9 +83,9 @@
       }
     },
 
-    provide(){
+    provide() {
       return {
-        $_ais : this.$instantsearch
+        $_ais: this.$instantsearch
       }
     },
 
